b6054a287e506e9ee4f659da35e0974884e1b6cb
dogan2026 <yigit8101996@gmail.com>
2026-04-05 13:24:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'kpss-cache-v3';
+const urlsToCache = [
+  './kpss-asistan.html',
+  './logo.png',
+  './manifest.json',
+  './sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
